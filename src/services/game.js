64674c5ec1dc94848981bc2ef887c0ef316aa5d5
kneauxn <kneauxn@gmail.com>
2016-12-13 16:40:42 -0700

    Working my way through the video I have outlined the CRUD

--- src/services/game.js
+++ src/services/game.js
@@ -7,24 +7,24 @@ export default class {
     // Setting a shortcut for db.games
     this.collection = db[COLLECTION];
   }
-  
+  // to return the entire collection
   all() {
-    
+    return thi.collection.find({});
   }
-  
+  // to return one doc by ID
   byId(id) {
-    
+    return this.collection.findOne({_id: id});
   }
   
   create(question = {}) {
-    
+    return this.collection.insert(game);
   }
   
   update(id, question = {}) {
-    
+    return this.collection.update({_id: id}, {$set: game});
   }
   
   remove(id) {
-    
+    return this.collection.remote({_id: id}, true)
   }
 };
